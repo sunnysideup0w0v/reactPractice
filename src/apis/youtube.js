8b0b8d23960ed7126b3402bb72fdaf21ddcb768e
sunnysideup0w0v <723973972538@naver.com>
2020-10-08 13:31:25 +0900

    Remove: api key remove

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const KEY = "AIzaSyDP4T_99jRDAnfGeNik5ujsDiPiYMpm7-g";
-
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
